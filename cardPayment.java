1e991d199556a8335039855d5e73220dafc783bf
21Seta <129466726+21Seta@users.noreply.github.com>
2025-12-12 17:35:37 +0400

    add cardPayment.java

--- cardPayment.java
+++ cardPayment.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class cardPayment {
+public class cardPayment extends Payment{
+   String paymentMethod(){
+        return "გადახდა მოხდა ბარათით თქვენ შეიძინეთ 100 ლარზე ნაკლები პროდუქცია და თქვენ არ გეკუთვნით ფასდაკლება ";
+    }
 }
